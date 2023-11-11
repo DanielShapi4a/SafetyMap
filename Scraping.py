be2714ab4fd7530fc22f995c466ee9f9eb42fa92
Daniel Shapira <danielshapira3@gmail.com>
2023-11-11 12:58:22 +0200

    <add> Scrape for 1 whole page ready

--- Scraping.py
+++ Scraping.py
@@ -3,6 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -30,31 +31,37 @@ wait = WebDriverWait(driver, 10)
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, "html.parser")
 
+# Find the main container div
 results_div = soup.find("div", class_="col-12 px-3 col-lg-8")
 
-# Find all the `<div>` elements with the class `"row row-gov"`
-row_divs = results_div.find_all("div", class_="row row-gov")
+# Find all the `<div>` elements with the class `"row row-gov ordered-fields"`
+row_divs = results_div.find_all("div", class_="row row-gov ordered-fields")
 
 # Extract the relevant information from each `<div>` element
-for row_div in row_divs:
+for index,row_div in enumerate (row_divs,start=1):
+    print(f"Block number {index}")
     try:
-        # Extract the country of the travel warning
-        country_span = row_div.find("span", class_="mr-1 ng-binding ng-scope")
-        country = country_span.text.strip()
+        # Find all the nested `<div>` elements with the class "col-12 col-sm-4 xs-p-0 xs-mb-10 xs-pe-20"
+        info_divs = row_div.find_all("div", class_="col-12 col-sm-4 xs-p-0 xs-mb-10 xs-pe-20")
 
-        # Extract the travel advisory level of the travel warning
-        advisory_level_span = row_div.find("span", class_='mr-1 xs-me-10 error-txt ng-binding')
-        advisory_level = advisory_level_span.text.strip()
+        # Iterate through each nested div to extract information
+        for info_div in info_divs:
+            # Extract information from <span> or <label> elements within the nested div
+            span_text = info_div.find("span").text.strip() if info_div.find("span") else ""
+            label_text = info_div.find("label").text.strip() if info_div.find("label") else ""
 
-        # Print the extracted information
-        print(f"Country: {country}")
-        print(f"Advisory Level: {advisory_level}")
-        print("--------------------------------")
+            # Print the extracted information
+            print(f"Span Text: {span_text}")
+            print(f"Label Text: {label_text}")
+
 
     except (AttributeError, IndexError) as e:
-        # Handle errors gracefully if country or advisory level not found
+        # Handle errors gracefully if information not found
         print(f"Error extracting data: {e}")
         print("--------------------------------")
+    print("--------------------------------")
 
 # Close the Chrome WebDriver when done
 driver.quit()
+
+

6f1ac95bb4fc247267fa287566ab4fdf016d7e65
Daniel Shapira <danielshapira3@gmail.com>
2023-11-15 00:00:28 +0200

    <add> trying to fix the data to encode correctly

--- CSVchanging.py
+++ CSVchanging.py
@@ -4,7 +4,7 @@ input_file_path = 'scraped.csv'
 output_file_path = 'Updated_Changed_CSV.csv'
 
 # Define the column names
-column_names = ["Country", "warning level", "recommendation", "the threatened area", "detail", "recommendations"]
+column_names = ["Country", "warning level", "recommendation", "the threatened area", "detail"]
 
 # Initialize variables to store data
 current_country_data = []
@@ -19,11 +19,14 @@ with open(input_file_path, 'r', encoding='utf-8') as input_file:
     next(csv_reader)  # Skip the second row
     
     for row in csv_reader:
-        current_country_data.append(row)
+        # Process each element in the row to remove irrelevant words and commas
+        cleaned_row = [element.rsplit(',', 1)[0].strip() for element in row]
+        current_country_data.append(cleaned_row)
+        
         # Check if we have collected data for one country (6 rows)
         if len(current_country_data) == 6:
             # Append the data for one country as a single row with column names
-            transformed_data.append(dict(zip(column_names, [item for sublist in current_country_data for item in sublist])))
+            transformed_data.append(dict(zip(column_names, [item[0] for item in current_country_data])))
             current_country_data = []
 
 # Write the transformed data to the output file
@@ -31,3 +34,4 @@ with open(output_file_path, 'w', encoding='utf-8', newline='') as output_file:
     csv_writer = csv.DictWriter(output_file, fieldnames=column_names)
     csv_writer.writeheader()
     csv_writer.writerows(transformed_data)
+    

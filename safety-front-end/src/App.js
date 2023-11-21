d0b8a841106563284f84596d9182aec14ad49679
Daniel Shapira <danielshapira3@gmail.com>
2023-11-21 21:57:00 +0200

    <add>

---
+++ safety-front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataDisplay from './components/DataDisplay';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Your Main App</h1>
+      <DataDisplay />
+    </div>
+  );
+};
+
+export default App;

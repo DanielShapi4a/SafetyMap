da706e2da470791e1fd6781f47d2dd1cb6366570
Daniel Shapira <danielshapira3@gmail.com>
2023-11-30 00:15:09 +0200

    random fix

--- safety-front-end/src/components/CountryInfoPanel.jsx
+++ safety-front-end/src/components/CountryInfoPanel.jsx
@@ -9,7 +9,7 @@ const CountryInfoPanel = ({ countryData }) => {
   }
 
   return (
-    <div className="country-info-panel">
+    <div className="country-info-panel" style={{color:"black"}}>
       <h2>{countryData.Country}</h2>
       <p>Warning Level: {countryData['warning level']}</p>
       <p>Recommendation: {countryData.recommendation}</p>

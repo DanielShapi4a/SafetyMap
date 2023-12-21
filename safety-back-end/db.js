080af4d5f0de1f5fff9f1fb8ab12d51761e44048
boaz1e <boazy@my.hit.ac.il>
2023-12-21 15:05:16 +0200

    Removed unnecessary files

--- safety-back-end/db.js
+++ safety-back-end/db.js
@@ -1,19 +1,20 @@
 // db.js
 require("dotenv").config();
-const { MongoClient } = require('mongodb');
-console.log(process.env.USER_NAME, process.env.PASSWORD);
-const uri =
-  `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.xqlbyho.mongodb.net/safety-info`;
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.xqlbyho.mongodb.net/safety-info`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const connectToDatabase = async () => {
   try {
     await client.connect();
-    console.log('Connected to MongoDB');
-    return client.db('safety-info').collection('safety');
+    console.log("Connected to MongoDB");
+    return client.db("safety-info").collection("safety");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
     throw error;
   }
 };
